1748a50e0645f526e27a4e6bb0d58ef10cc3084d
Marco Yeung <myeung@redhat.com>
2021-07-08 07:25:50 -0400

    learning

--- working/greeting/src/main/java/org/acme/GreetingResource.java
+++ working/greeting/src/main/java/org/acme/GreetingResource.java
@@ -4,11 +4,15 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @Path("/greeting")
 public class GreetingResource {
+    @ConfigProperty(name="greetings")
     List<String> greetings = new ArrayList<>(); // (1)
 
     @GET
